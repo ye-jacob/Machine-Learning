22d90d5668045a8cb47eed29de4e93635d9e1c84
Jacob Ye <ye.jacob@gmail.com>
2025-06-27 23:50:51 -0400

    new updates

--- personl-project/ionospherepy.py
+++ personl-project/ionospherepy.py
@@ -1,12 +1,49 @@
 
 # load the ionosphere dataset and summarize the shape
 from pandas import read_csv
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
 from matplotlib import pyplot
 # define the location of the dataset
 url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/ionosphere.csv'
 # load the dataset
 df = read_csv(url, header=None)
 # summarize shape
-print(df.describe())
-df.hist()
+X, y = df.values[:, :-1], df.values[:, -1]
+# ensure all data are floating point values
+X = X.astype('float32')
+# encode strings to integer
+y = LabelEncoder().fit_transform(y)
+# split into train and test datasets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+# determine the number of input features
+n_features = X.shape[1]
+# define model
+model = Sequential()
+model.add(Dense(10, activation='relu', kernel_initializer='he_normal', input_shape=(n_features,)))
+model.add(Dense(1, activation='sigmoid'))
+# compile the model
+model.compile(optimizer='adam', loss='binary_crossentropy')
+# fit the model
+history = model.fit(X_train, y_train, epochs=200, batch_size=32, verbose=0, validation_data=(X_test,y_test))
+# predict test set
+yhat = model.predict_classes(X_test)
+# evaluate predictions
+score = accuracy_score(y_test, yhat)
+print('Accuracy: %.3f' % score)
+# plot learning curves
+pyplot.title('Learning Curves')
+pyplot.xlabel('Epoch')
+pyplot.ylabel('Cross Entropy')
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='val')
+pyplot.legend()
 pyplot.show()
+
+
+#print(df.describe())
+#df.hist()
+#pyplot.show()
