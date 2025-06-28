484ef59d8ded673f5480c4cd1d33bdd7077ec448
Jacob Ye <ye.jacob@gmail.com>
2025-06-27 23:45:31 -0400

    pandas

--- personl-project/ionospherepy.py
+++ personl-project/ionospherepy.py
@@ -1,9 +1,12 @@
 
 # load the ionosphere dataset and summarize the shape
 from pandas import read_csv
+from matplotlib import pyplot
 # define the location of the dataset
 url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/ionosphere.csv'
 # load the dataset
 df = read_csv(url, header=None)
 # summarize shape
-print(df.shape)
+print(df.describe())
+df.hist()
+pyplot.show()
